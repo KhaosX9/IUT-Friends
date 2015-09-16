140100ec5e866c58bec1e4c58b523bfc477eafd3
Khaos <khaosx9@hotmail.fr>
2015-09-16 20:27:17 +0200

    Test.

---
+++ Test/src/iut/info2/test/Letest.java
@@ -0,0 +1,22 @@
+/*
+ * Letest.java                                16 sept. 2015
+ * IUT RODEZ INFO1 Groupe 3 2014-2015
+ * 
+ */
+package iut.info2.test;
+
+/** TODO commenter la responsabilité de cette classe
+ * @author Nathan RIBAS
+ *
+ */
+public class Letest {
+
+	/** TODO commenter le rôle de la méthode
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("voila");
+
+	}
+
+}
